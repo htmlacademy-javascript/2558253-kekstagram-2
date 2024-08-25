6e97eacbc75222ad70357d4e56beaa5e46a0fbe6
Keks <keks@htmlacademy.ru>
2024-08-25 12:10:39 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,29 +1,19 @@
-function checkLength (inputString, maxLength){
-  return inputString.length <= maxLength;
-}
+const checkLength = (inputString = '', maxLength = 1) => inputString.length <= maxLength;
 console.log(checkLength('проверяемая строка', 20));
 
-const checkPalindrome = function (string) {
-  string = string.replaceAll(' ', '');
-  const normString = string.toLowerCase();
-  let newString = '';
-  for (let i = (normString.length - 1); i >= 0; i--){
-    newString += normString[i];
+const checkPalindrome = (string = '') => {
+  const normString = string.replaceAll(' ','').toLowerCase();
+  for (let i = 0; i < Math.floor(normString.length / 2); i++) {
+    if (normString[i] !== normString[normString.length - 1 - i]) {
+      return false;
+    }
   }
-  return normString === newString;
+  return true;
 };
 console.log(checkPalindrome('Лёша на полке клопа нашёл'));
 
-const getNumbers = (string) => {
-  let finalNumber = '';
-  for (let i = 0; i <= string.length; i++) {
-    let sepIndex = parseInt(string[i], 10);
-    if (Number.isNaN(sepIndex)){
-      continue;
-    } else {
-      finalNumber += sepIndex;
-    }
-  }
+const getNumbers = (string = '') => {
+  const finalNumber = string.toString().replace(/\D/g, '');
   return parseInt(finalNumber, 10);
 };
 console.log(getNumbers('1 кефир, 0.5 батона'));
